25083a9dee94ae1b8e67b9771ee0a97ffd4b6e31
ArtemMykholap <56889896+ArtemMykholap@users.noreply.github.com>
2020-04-17 16:05:32 +0300

    pzdc

--- js/lesson3/task1/index.js
+++ js/lesson3/task1/index.js
@@ -1,42 +1,42 @@
 // Унарный плюс приводит операнд к числу
 +17; +
 '77';
-console.log("+17:", 17);
-console.log("+'77':", 77);
+console.log("+17: ", 17);
+console.log("+'77': ", 77);
 
 // Бинарный плюс складывает строки или прибавляет числа
 10 + '5';
 'some' + 'text';
-console.log("10+'5' :", 10 + '5');
+console.log("10+'5': ", 10 + '5');
 
 // Математические операции с undefined возвращают NaN
 undefined + 1;
-console.log("undefined + 1 :", undefined + 1);
+console.log("undefined + 1: ", undefined + 1);
 
 null + 8;
-console.log("null + 8:", null + 8);
+console.log("null + 8: ", null + 8);
 
 undefined + '5';
-console.log("undefined + '5':", undefined + '5');
+console.log("undefined + '5': ", undefined + '5');
 
 
 7 - null;
-console.log("7 - null : ", 7 - null);
+console.log("7 - null :", 7 - null);
 5 - undefined;
-console.log("5 - undefined : ", 5 - undefined)
+console.log("5 - undefined: ", 5 - undefined)
 
 
 'text' + null;
-console.log("'text' + null : ", 'text' + null);
+console.log("'text' + null: ", 'text' + null);
 
 
 let num = 17;
-console.log("++num : ", ++num);
-console.log("num++ : ", num++);
+console.log("++num: ", ++num);
+console.log("num++: ", num++);
 
 const a = 17;
 const b = -a;
-console.log('-a:', b);
+console.log('-a: ', b);
 
 
 '24' / 4;
