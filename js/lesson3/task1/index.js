7723bf6292ee294f5c4aa53ee88d526be361bf6b
ArtemMykholap <56889896+ArtemMykholap@users.noreply.github.com>
2020-04-17 15:59:37 +0300

    rere

--- js/lesson3/task1/index.js
+++ js/lesson3/task1/index.js
@@ -34,16 +34,17 @@ let num = 17;
 console.log("++num : ", ++num);
 console.log("num++ : ", num++);
 
-
-// математические операторы приводят операнды к числам
 const a = 17;
 const b = -a;
+console.log('-a:', b);
+
+
 '24' / 4;
-console.log("'24' / 4 : ", '24' / 4)
+console.log("'24' / 4 : ", '24' / 4);
 
 
 18 * '3';
-console.log("18 * '3' : ", 18 * '3')
+console.log("18 * '3' : ", 18 * '3');
 
 
 '9' - '3';
@@ -51,29 +52,28 @@ console.log("'9' - '3' : ", '9' - '3');
 
 
 'seventeen' / 3;
-console.log("'seventeen' / 3 : ", 'seventeen' / 3)
+console.log("'seventeen' / 3: ", 'seventeen' / 3);
 
 
 4 * 'four';
-console.log("4 * 'four' : ", 4 * 'four')
+console.log("4 * 'four' :", 4 * 'four');
 
 17 / 0;
-console.log("17 / 0 : ", 17 / 0)
-
+console.log("17 / 0 :", 17 / 0);
 10 + 'seven';
-console.log("10 + 'seven' : ", 10 + 'seven');
+console.log("10 + 'seven' :", 10 + 'seven');
 
 
 undefined + 1;
-console.log("undefined + 1 : ", undefined + 1);
+console.log("undefined + 1 :", undefined + 1);
 
 
 null + 8;
-console.log("null + 8 : ", null + 8);
+console.log("null + 8 :", null + 8);
 
 
 undefined + '5';
-console.log("undefined + '5' : ", undefined + '5')
+console.log("undefined + '5' :", undefined + '5');
 
 'text' + null;
-console.log("'text' + null : ", 'text' + null);
+console.log("'text' + null :", 'text' + null);
