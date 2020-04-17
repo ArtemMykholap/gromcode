2c1db278470e2cba62973603f5a1ca82e72362ce
ArtemMykholap <56889896+ArtemMykholap@users.noreply.github.com>
2020-04-17 12:40:26 +0300

    summary

---
+++ js/lesson2/task4/index.js
@@ -0,0 +1,9 @@
+let a = 4;
+let b = 2;
+console.log("a: ", a);
+console.log("b: ", b);
+let c = a - b;
+console.log('a - b =', c);
+console.log('a + b =', a + b);
+console.log('a / b =', a / b);
+console.log('a * b =', a * b);
