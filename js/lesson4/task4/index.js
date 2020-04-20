115bcb06fe2fd2a178694c7aa253daf125c803de
ArtemMykholap <56889896+ArtemMykholap@users.noreply.github.com>
2020-04-20 12:04:47 +0300

    23

--- js/lesson4/task4/index.js
+++ js/lesson4/task4/index.js
@@ -1,5 +1,15 @@
 let n = 10;
-let sum = 0;
-for (sum = 0; sum <= n; sum++) {
-    console.log(String(sum));
+let sum = '';
+for (i = 1; i <= n; i++) {
+    sum += String(i);
 }
+console.log(sum);
+
+
+
+
+/*let sum;
+for (let n = 1; n <= 10; n++) {
+    sum += String(n);
+}
+console.log(sum)*/
