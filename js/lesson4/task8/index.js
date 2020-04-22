750eb575f2282298ba63ec4753ec83f12190d0af
ArtemMykholap <56889896+ArtemMykholap@users.noreply.github.com>
2020-04-22 10:31:08 +0300

    цйуйцу

--- js/lesson4/task8/index.js
+++ js/lesson4/task8/index.js
@@ -2,7 +2,7 @@ let sum = 0;
 for (let i = 0; i <= 1000; i++) {
     sum += i;
 }
-let j = sum / 1234;
+let j = sum % 1234;
 let k = (j + '').split('.');
-let result = Number(k[0]) < Number(k[1]) ? 'true' : 'false';
+let result = Number(k[0]) > Number(k[1]) ? 'true' : 'false';
 console.log(result);
