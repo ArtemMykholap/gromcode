48e18a4835f55a9abfd01020a00e444cc9a28de4
ArtemMykholap <56889896+ArtemMykholap@users.noreply.github.com>
2020-04-22 11:45:08 +0300

    11

--- js/lesson4/task5/index.js
+++ js/lesson4/task5/index.js
@@ -2,18 +2,17 @@ let m = 10;
 let n = 50;
 let result = 0;
 for (let i = 10; i <= 50; i++) {
+    if (i % 4 !== 0 && i % 2 === 0) {
+        result += i;
+    }
+    if (i % 3 === 0) {
+        result -= i;
+    }
+    if (i % 4 === 0) {
+        result *= i;
+    }
     if (i % 5 === 0) {
         console.log(i);
-    } else {
-        if (i % 4 !== 0 && i % 2 === 0) {
-            result += i;
-        }
-        if (i % 3 === 0) {
-            result -= i;
-        }
-        if (i % 4 === 0) {
-            result *= i;
-        }
     }
 
 }
