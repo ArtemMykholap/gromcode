8cdd47a32c3c549d23ab6663b5be26c2df5a04a4
ArtemMykholap <56889896+ArtemMykholap@users.noreply.github.com>
2020-04-24 12:07:54 +0300

    1111

--- js/lesson5/task7/index.js
+++ js/lesson5/task7/index.js
@@ -1,9 +1,8 @@
 function increaser(index, a) {
     let c = 0;
     if (a > index) {
-        c = a + index;
-        а = c;
-        return a;
+        return a = a + index;
+
 
     }
     if (a < index) {
