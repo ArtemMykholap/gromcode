2425d6604ae0506bf4cfa3f6f9468e73dd0cb080
ArtemMykholap <56889896+ArtemMykholap@users.noreply.github.com>
2020-04-24 16:44:43 +0300

    1111

--- js/lesson5/task5/index.js
+++ js/lesson5/task5/index.js
@@ -1,25 +1,10 @@
-/*function getSum(a, b) {
+function getSum(a, b) {
     let result = 0;
 
-    for (i < a; i < b; i++) {
+    for (let i = a; i <= b; i++) {
         if (i % 2 === 0) {
             result += i;
         }
     }
     return result;
-}*/
-function simpleNumbers(start, end) {
-
-    for (var i = start, flag = false; i <= end; i++, flag = 0) {
-        for (var j = 2; j * 2 <= i; j++) {
-            if (i % j == 0) {
-                flag = true;
-                break;
-            }
-        }
-        if (!flag)
-            res[res.length] = i;
-    }
-    return res;
 }
-console.log(simpleNumbers(3, 20));
