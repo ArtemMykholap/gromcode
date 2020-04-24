e150c1657242c99cc6029b23b88ab1f6285f240d
ArtemMykholap <56889896+ArtemMykholap@users.noreply.github.com>
2020-04-24 17:28:59 +0300

    1221

---
+++ js/lesson5/task8/index.js
@@ -0,0 +1,13 @@
+function sum(from, to) {
+    let result = 0;
+    for (i = from; i <= to; i++) {
+        result += i;
+    }
+    return result;
+}
+
+
+function compareSums(a, b, c, d) {
+    let compare = sum(a, b) > sum(c, d);
+    return compare;
+}
