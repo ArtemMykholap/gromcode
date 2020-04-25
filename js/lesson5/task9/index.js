202433719036ed512c9636773429e81ac610204c
ArtemMykholap <56889896+ArtemMykholap@users.noreply.github.com>
2020-04-25 11:16:01 +0300

    1111

---
+++ js/lesson5/task9/index.js
@@ -0,0 +1,13 @@
+let emptyArray = [];
+
+function findDivCount(a, b, c) {
+    let newArray;
+    for (let i = a; i <= b; i++) {
+        if (i % c === 0) {
+
+            newArray = emptyArray.push(i);
+
+        }
+    }
+    console.log(emptyArray.length);
+}
