38d28c522917c5fa3ad22815062d482c04b61b81
ArtemMykholap <56889896+ArtemMykholap@users.noreply.github.com>
2020-05-07 14:31:33 +0300

    111

--- js/lesson6/hw5/index.js
+++ js/lesson6/hw5/index.js
@@ -3,9 +3,11 @@ const numbers = [10, 21, 50, 113, 115, 122];
 function reverseArray(numbers) {
     if (!Array.isArray(numbers)) {
         return null;
-    } else {
-        let reverse = [];
-        reverse = numbers.reverse();
-        console.log(reverse);
     }
+    let reverse = [];
+    for (let i = numbers.length - 1; i >= 0; i--) {
+        reverse.push(numbers[i]);
+    }
+    console.log(reverse);
+
 }
