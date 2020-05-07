88d88cf2948f88ab92316417d04cd5579c2d3e1e
ArtemMykholap <56889896+ArtemMykholap@users.noreply.github.com>
2020-05-07 09:16:10 +0300

    1211

--- js/lesson6/task3/index.js
+++ js/lesson6/task3/index.js
@@ -1,11 +1,11 @@
 function getSpecialNumbers(m, n) {
     let specialNumbers = [];
-    for (i = from; i <= to; i++) {
+    for (let i = m; i <= n; i++) {
         if (i % 3 === 0) {
             specialNumbers.push(i);
         }
     }
-    return specialNumbers
+    return specialNumbers;
 }
 const specialNumbers = getSpecialNumbers(10, 40);
 console.log(specialNumbers);
