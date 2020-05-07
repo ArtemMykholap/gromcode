00a6ae2deb05e1d2efd04d0e1fd8d8dad460c601
ArtemMykholap <56889896+ArtemMykholap@users.noreply.github.com>
2020-05-07 11:41:15 +0300

    111

--- js/lesson6/hw1/index.js
+++ js/lesson6/hw1/index.js
@@ -1,10 +1,9 @@
-const len = [1, 2, 3, 4, 5, 6];
-
-
 function createEmptyArray(len) {
-    var newArray = [];
-    for (var i = 0; i < len.length; i++) {
-        newArray.push("");
-    }
-    return newArray;
+    let emptyArray = [];
+
+    emptyArray.length = len;
+
+    return emptyArray;
+
+
 }
