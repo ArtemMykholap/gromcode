dc1ecb31c3b5155536af2d080c11c41fc74b1760
ArtemMykholap <56889896+ArtemMykholap@users.noreply.github.com>
2020-05-08 13:02:20 +0300

    111

---
+++ js/lesson6/hw13/index.js
@@ -0,0 +1,10 @@
+const numbers = [10, 10, 20, 50, 33, 21, 21, 27];
+
+function uniqueCount(numbers) {
+    if (!Array.isArray(numbers)) {
+        return null;
+    }
+    const unique = [...new Set(numbers)]
+    const count = unique.length;
+    console.log(count);
+}
