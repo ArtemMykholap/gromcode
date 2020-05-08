26ee511730519caa56bfab51ce6a2bc6bd35322e
ArtemMykholap <56889896+ArtemMykholap@users.noreply.github.com>
2020-05-08 19:10:34 +0300

    1111

--- js/lesson6/hw8/index.js
+++ js/lesson6/hw8/index.js
@@ -2,6 +2,29 @@ let numbers = [15, 27, 18, 24, 13];
 
 
 
+
+
+
+
+
+
+function sortDesc(numbers) {
+    if (!Array.isArray(numbers)) {
+        return null;
+    }
+    for (let i = 0,
+            len = numbers.length - 1; i < len; i++) {
+        for (let j = 0, lenJ = len - i; j < lenJ; j++) {
+            if (numbers[j] > numbers[j - 1]) {
+                let trim = numbers[j];
+                numbers[j] = numbers[j - 1];
+                numbers[j - 1] = trim;
+            }
+        }
+    }
+    return numbers;
+}
+
 function sortAsc(numbers) {
     if (!Array.isArray(numbers)) {
         return null;
@@ -18,22 +41,5 @@ function sortAsc(numbers) {
         }
     }
 
-    return numbers;
-}
-
-function sortDesc(numbers) {
-    if (!Array.isArray(numbers)) {
-        return null;
-    }
-    for (let i = 0,
-            len = numbers.length - 1; i < len; i++) {
-        for (let j = 0, lenJ = len - i; j < lenJ; j++) {
-            if (numbers[j] > numbers[j - 1]) {
-                let trim = numbers[j];
-                numbers[j] = numbers[j - 1];
-                numbers[j - 1] = trim;
-            }
-        }
-    }
     return numbers;
 }
