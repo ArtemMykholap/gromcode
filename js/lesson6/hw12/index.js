5672e0927e1ef4abeb2a68e1401bfe4129d97e4c
ArtemMykholap <56889896+ArtemMykholap@users.noreply.github.com>
2020-05-08 10:44:19 +0300

    111

---
+++ js/lesson6/hw12/index.js
@@ -0,0 +1,13 @@
+const numbers = 1;
+
+function removeDuplicates(numbers) {
+    if (!Array.isArray(numbers)) {
+        return null;
+    }
+    const uniqArray = [...new Set(numbers)];
+
+
+    console.log(uniqArray);
+
+
+}
