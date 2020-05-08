b5edf1bd0bdceaec76cfb1392882a55675b92e6b
ArtemMykholap <56889896+ArtemMykholap@users.noreply.github.com>
2020-05-08 16:40:33 +0300

    1111

---
+++ js/lesson6/hw9/index.js
@@ -0,0 +1,29 @@
+const clients = ['John', 'Mac', "Merry"];
+const balances = [1000, 570, 700];
+let client = 'Mac';
+let amount = 2000;
+
+
+function withdraw(clients, balances, client, amount) {
+    let rest = 0;
+    for (let i = 0; i < clients.length; i++) {
+
+
+        if (clients[i] === client) {
+
+            if (balances[i] < amount) {
+                return -1;
+
+            } else if (balances[i] > amount) {
+                rest = balances[i] - amount;
+                balances[i] = rest;
+            }
+
+
+
+        }
+
+    }
+    console.log(rest);
+
+}
