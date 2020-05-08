645cda9d1fcfe8b2eab7f98dfe4440eb1e07b4c8
ArtemMykholap <56889896+ArtemMykholap@users.noreply.github.com>
2020-05-08 12:03:41 +0300

    111

--- js/lesson6/hw11/index.js
+++ js/lesson6/hw11/index.js
@@ -1,13 +1,15 @@
 let numbers = [15, 27, 18, 24, 13];
-let len = 3;
+let len = 1;
 
 
 function includes(numbers, len) {
+    let a = 0;
     for (let i = 0; i < numbers.length; i++) {
         if (numbers[i] === len) {
-            console.log(true);
+            a += 1;
         }
-        console.log(false);
+        console.log(a > 0);
     }
+
 
 }
