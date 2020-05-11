41298f09f7670c30635d10d71194c18b7fdedc0b
ArtemMykholap <56889896+ArtemMykholap@users.noreply.github.com>
2020-05-11 12:46:23 +0300

    111

--- js/lesson7/task1/index.js
+++ js/lesson7/task1/index.js
@@ -1,15 +1,13 @@
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-
 const getSpecialNumbers = numbers => {
-    let SpecialNumbers = [];
+        let SpecialNumbers = [];
 
-    function checkIfSpecialNumber(num) {
-        if (num % 3 === 0) {
-            SpecialNumbers.push(num);
+        function checkIfSpecialNumber(num) {
+            if (num % 3 === 0) {
+                SpecialNumbers.push(num);
+            }
         }
+        numbers.forEach(checkIfSpecialNumber);
+        return SpecialNumbers;
     }
-    numbers.forEach(checkIfSpecialNumber);
-    return SpecialNumbers;
-}
-const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-console.log(getSpecialNumbers(arr));
+    // const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    // console.log(getSpecialNumbers(arr));
