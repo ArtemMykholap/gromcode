e31a11366df97f5286805232149158f161564b89
ArtemMykholap <56889896+ArtemMykholap@users.noreply.github.com>
2020-05-11 14:09:28 +0300

    2111

---
+++ js/lesson7/task3/index.js
@@ -0,0 +1,9 @@
+const flatArray = arr => {
+    const flatArray = arr
+        .reduce((acc, elem) => {
+            return acc.concat(elem);
+        }, []);
+    return flatArray
+};
+const initArray = [1, [2, 3, 4], 5, 6];
+console.log(flatArray(initArray));
