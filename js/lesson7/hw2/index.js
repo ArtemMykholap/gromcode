6dd0a53f23228b06eebf2866dacc2b283ffe1ed9
ArtemMykholap <56889896+ArtemMykholap@users.noreply.github.com>
2020-05-11 18:27:59 +0300

    111

--- js/lesson7/hw2/index.js
+++ js/lesson7/hw2/index.js
@@ -1,12 +1,8 @@
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-
-
 const reverseArray = numbers => {
     if (!Array.isArray(numbers)) {
         return null;
     } else {
-        numbers.reverse();
-        return numbers;
+        return numbers.slice().reverse();
 
 
     };
