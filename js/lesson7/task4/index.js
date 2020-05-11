4b193f9fd35df2c44368d0851faec45fb7f9a0a0
ArtemMykholap <56889896+ArtemMykholap@users.noreply.github.com>
2020-05-11 14:50:45 +0300

    111

--- js/lesson7/task4/index.js
+++ js/lesson7/task4/index.js
@@ -1,4 +1,4 @@
 const getMessagesForBestStudents = (allStudents, failedStudents) =>
     allStudents
     .filter(name => !failedStudents.includes(name))
-    .map(name => 'Good job. ' + name);
+    .map(name => 'Good job, ' + name);
