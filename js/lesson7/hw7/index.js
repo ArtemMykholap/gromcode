2053bb5776563fe248ec2ef85428fd83deb08d5b
ArtemMykholap <56889896+ArtemMykholap@users.noreply.github.com>
2020-05-11 21:54:59 +0300

    1111

---
+++ js/lesson7/hw7/index.js
@@ -0,0 +1,11 @@
+const names = ['John', 'Olivya', 'Oleksandr', 'Emanuel', 'Vanya', 'Nastya'];
+const text = 'ya';
+let arr = [];
+
+names.filter(function(names, text) {
+    if (names.length > 5)
+        if (names == ) {
+            arr++;
+        }
+    console.log(arr);
+});
---
+++ js/lesson7/hw8/index.js
@@ -0,0 +1,4 @@
+function arrAverage(nums) {
+    if (!Array.isArray(arr)) return null;
+    return nums.reduce((a, b) => (a + b)) / nums.length;
+}
