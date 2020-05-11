d38b40cbc439f420faab72fd34130a79e0f63761
ArtemMykholap <56889896+ArtemMykholap@users.noreply.github.com>
2020-05-11 19:47:10 +0300

    11111

--- js/lesson7/hw6/index.js
+++ js/lesson7/hw6/index.js
@@ -1,15 +1,6 @@
-const sumNumber = [1, 2, 3, 4, 20, 40];
-
-
-const sum = numbers => {
-
-    if (!Array.isArray(numbers)) {
-        return null;
-    }
-
-    const result = sumNumber.reduce((a, b) => {
-        return a + b;
-    });
-
-    return result;
-};
+function sum(arr) {
+    if (!Array.isArray(arr)) return null;
+    return arr.reduce((a, b) => a + b);
+}
+let numbers = [1, 2, 3, 4, 5];
+console.log(sum(numbers));
