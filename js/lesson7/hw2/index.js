967d413d154fc699b040f598e6284a4ca4ca2d72
ArtemMykholap <56889896+ArtemMykholap@users.noreply.github.com>
2020-05-11 17:34:08 +0300

    211

--- js/lesson7/hw2/index.js
+++ js/lesson7/hw2/index.js
@@ -1,7 +1,7 @@
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 let container = [];
 
-const squareArray = numbers => {
+const reverseArray = numbers => {
     if (!Array.isArray(numbers)) {
         return null;
     } else {
