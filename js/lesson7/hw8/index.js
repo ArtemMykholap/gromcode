6aec66540f407c78e02a14567d2e7b4bcb279fc1
ArtemMykholap <56889896+ArtemMykholap@users.noreply.github.com>
2020-05-11 22:01:05 +0300

    1111

--- js/lesson7/hw8/index.js
+++ js/lesson7/hw8/index.js
@@ -1,4 +1,6 @@
 function arrAverage(nums) {
-    if (!Array.isArray(arr)) return null;
+    if (!Array.isArray(nums)) return null;
     return nums.reduce((a, b) => (a + b)) / nums.length;
-}
+};
+let numbers = [1, 2, 3, 4, 5];
+console.log(arrAverage(numbers));
