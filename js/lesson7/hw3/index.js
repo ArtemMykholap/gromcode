f22e7999fa5afa5b8264f094b385472357a5badd
ArtemMykholap <56889896+ArtemMykholap@users.noreply.github.com>
2020-05-12 20:17:16 +0300

    111

--- js/lesson7/hw3/index.js
+++ js/lesson7/hw3/index.js
@@ -1,28 +1,10 @@
-// let arr = [1, 2, 3, 4, 5];
-// let delta = 20;
-
-
-// const increaseEvenEl = (arr, delta) => {
-//     const arr = arr.map((even) => {
-//             return even % 2 === 0
-//                 // return even + delta;
-
-//         })
-//         // return arr;
-//     console.log(arr);
-// }
-// console.log(arr);
-
-
 var arr = [1, 2, 3, 4, 5, 6, 7, 8];
 let delta = 20;
 
 
 const increaseEvenEl = (arr, delta) => {
+    if (!Array.isArray(arr)) return null;
 
-    // .filter((even) => {
-    //     return even % 2 == 0;
-    // })
     let star = arr.map((even) => {
 
         if (even % 2 == 0) {
