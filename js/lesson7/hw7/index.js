3aa25cc7cfa9f230c32d3ddf319dde3fa39428fa
ArtemMykholap <56889896+ArtemMykholap@users.noreply.github.com>
2020-05-12 16:44:04 +0300

    111

--- js/lesson7/hw7/index.js
+++ js/lesson7/hw7/index.js
@@ -1,10 +1,15 @@
 const names = ['John', 'Olivya', 'Oleksandr', 'Emanuel', 'Vanya', 'Nastya'];
 const text = 'ya';
-let arr = [];
 
-const filterNames = (names, text) = names.filter()
-if (names.length > 5)
-    if (names === text) {
-        arr++;
-    }
-console.log(arr);;
+const filterNames = (names, text) => {
+    const arr = names.filter((name) => {
+        return name.length > 5 && name.includes(text);
+
+
+
+    })
+    return arr;
+
+}
+
+console.log(filterNames(names, text));
