ecbebcd66a888a44da9ca2d66f6a02da782f78db
ArtemMykholap <56889896+ArtemMykholap@users.noreply.github.com>
2020-05-13 15:43:58 +0300

    1111

--- js/lesson8/task1/index.js
+++ js/lesson8/task1/index.js
@@ -1,10 +1,11 @@
-const arr = ['a', 17.1, 'John Doe'];
-const thransformToObject = arr => {
+const transformToObject = arr => {
     let obj = {};
+
     arr.forEach(el => obj[el] = el);
+
     return obj;
 };
 
-
+const arr = ['a', 17.1, 'John Doe']
 const result = transformToObject(arr);
 console.log(result);
