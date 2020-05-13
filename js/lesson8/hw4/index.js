008ddd79ba091792e01819e93d66ebac01084b7a
ArtemMykholap <56889896+ArtemMykholap@users.noreply.github.com>
2020-05-13 19:23:31 +0300

    11111

---
+++ js/lesson8/hw4/index.js
@@ -0,0 +1,31 @@
+const userData = {
+    name: 'Tom',
+    age: 17,
+};
+
+userId = '11111';
+
+const addPropertyV1 = (userData, userId) => {
+    userData.id = userId;
+    return userData;
+}
+
+const addPropertyV2 = (userData, userId) => {
+    const party = { id: userId, };
+    return Object.assign(userData, party);
+
+}
+
+const addPropertyV3 = (userData, userId) => {
+    userData.id = userId;
+    let userDataCopy = Object.assign({}, userData)
+    return userDataCopy;
+}
+
+const addPropertyV4 = (userData, userId) => {
+
+    let userDataCopy = {...userData };
+    userDataCopy.id = userId;
+    let userDataNew = {...userData, ...userDataCopy };
+    return userDataNew;
+}
