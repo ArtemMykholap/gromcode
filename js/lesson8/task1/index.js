d01395b4b165fa3b3fc81ddc23ed5a57b6d554d1
ArtemMykholap <56889896+ArtemMykholap@users.noreply.github.com>
2020-05-13 15:28:07 +0300

    1111

--- js/lesson8/task1/index.js
+++ js/lesson8/task1/index.js
@@ -1,11 +1,7 @@
 const arr = ['a', 17.1, 'John Doe'];
 const thransformToObject = arr => {
     let obj = {};
-    arr.forEach(el => {
-
-        obj[el] = el;
-    });
-
+    arr.forEach(el => obj[el] = el);
     return obj;
 };
-// console.log(thransformToObject(arr));
+console.log(thransformToObject(arr));
