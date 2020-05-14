27c5baf858f086ccc655cc0d84bd7a6bc5000760
ArtemMykholap <56889896+ArtemMykholap@users.noreply.github.com>
2020-05-14 12:43:26 +0300

    1111

---
+++ js/lesson8/hw7/index.js
@@ -0,0 +1,18 @@
+const obj1 = {
+    name: 'T1om',
+    student: true,
+};
+const obj2 = {
+    name: 'Tom',
+    student: true,
+};
+
+let conpareObjects = (obj1, obj2) => {
+    const entriesArr1 = Object.entries(obj1).flat();
+    const entriesArr2 = Object.entries(obj2).flat();
+    // if (entriesArr1.length !== entriesArr2.length) {
+    //     return false;
+    // };
+    return entriesArr1.toString() === entriesArr2.toString();
+
+}
