5e468bb6cd7aaed453a38d69d9e7a46be2cf2eba
ArtemMykholap <56889896+ArtemMykholap@users.noreply.github.com>
2020-05-14 11:05:07 +0300

    11111

--- js/lesson8/hw6/index.js
+++ js/lesson8/hw6/index.js
@@ -12,44 +12,22 @@ const obj2 = {
 
 
 
-const addPropertyV1 = (obj1, obj2) => {
-    return Object.assign(obj1, obj2);
+const mergeObjectsV1 = (obj1, obj2) => {
+    let newObj = Object.assign({}, obj1, obj2);
+    return newObj;
 };
 
-const addPropertyV2 = (obj1, obj2) => {
-    return Object.assign(obj2, obj1);
+const mergeObjectsV2 = (obj1, obj2) => {
+    let newObj = Object.assign({}, obj2, obj1);
+    return newObj;
 };
-
-
-// const addPropertyV1 = (obj1, key, value) => {
-//     obj[key] = value;
-//     return obj;
-// };
-
-// const addPropertyV2 = (obj, key, value) => {
-//     const party = {
-//         [key]: value,
-//     };
-//     return Object.assign(obj, party);
-// }
-
-
-// const addPropertyV3 = (obj, key, value) => {
-//     const party = {
-//         [key]: value,
-//     };
-//     let objCopy = Object.assign({}, obj, party);
-//     return objCopy;
-// };
-
-// const addPropertyV4 = (obj, key, value) => {
-
-//     let objCopy = {
-//         [key]: value
-//     };
-
-//     return Object.assign({...obj }, objCopy);
-
-// };
-// const result = addPropertyV4(obj, 'literature', 'post-modern');
-// console.log(result);
+const mergeObjectsV3 = (obj1, obj2) => {
+    let newObj = Object.assign({...obj1 }, {...obj2 })
+    return newObj;
+}
+const mergeObjectsV4 = (obj1, obj2) => {
+    let newObj = Object.assign({...obj2 }, {...obj1 })
+    return newObj;
+}
+const result = mergeObjectsV4(obj1, obj2);
+console.log(result);
