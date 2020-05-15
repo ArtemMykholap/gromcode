ef710a245ba5d8a3b93f1a48abcc76d63fd6b5d8
ArtemMykholap <56889896+ArtemMykholap@users.noreply.github.com>
2020-05-15 17:04:39 +0300

    1111

---
+++ js/lesson9/hw2/index.js
@@ -0,0 +1,27 @@
+rooms = {
+    room1: [
+        { name: 'room1 name1' },
+        { name: 'room1 name2' },
+        { name: 'room1 name3' },
+        { name: 'room1 name4' },
+
+    ],
+    room2: [
+        { name: 'room2 name1' },
+
+    ],
+    room3: [
+        { name: 'room3 name1' },
+        { name: 'room3 name2' },
+        { name: 'room3 name3' },
+
+    ],
+
+};
+
+const getPeople = object => {
+    let arr = Object.values(object).flat().map(({ name }) => name);
+    return arr;
+
+}
+getPeople(rooms);
