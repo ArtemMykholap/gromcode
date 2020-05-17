98ff8370f6ab5f384506055b5ecdf700f09a462f
ArtemMykholap <56889896+ArtemMykholap@users.noreply.github.com>
2020-05-17 23:22:04 +0300

    1111

---
+++ js/lesson11/hw1/index.js
@@ -0,0 +1,28 @@
+// input:String
+// output:array of String
+
+
+
+const text = 'abcdefggsdfkdsiuew8372479237uwer';
+let n;
+
+const splitText = (text, n = 10) => {
+    if (typeof text !== 'string') {
+        return null;
+    }
+    let newArr = [];
+    let startPosition = 0;
+    while (true) {
+        let chunk = text.substr(startPosition, n);
+        if (chunk.length === 0) {
+            break;
+        }
+        if (chunk.length < n) {
+            chunk = chunk + '.'.repeat(n - chunk.length);
+        }
+        newArr.push(chunk[0].toUpperCase() + chunk.slice(1));
+        startPosition += n;
+    }
+    return newArr.join('\n');
+}
+console.log(splitText(text, n));
