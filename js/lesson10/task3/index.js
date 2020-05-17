c7213e9685d5178ff475c492a891442ca73188d3
ArtemMykholap <56889896+ArtemMykholap@users.noreply.github.com>
2020-05-17 14:19:31 +0300

    1111

---
+++ js/lesson10/task3/index.js
@@ -0,0 +1,9 @@
+//input:number
+//output:array of numbers
+
+let number = 15.77;
+const multiRound = number => [
+    Math.floor(number), Math.round(number), Math.ceil(number), Math.trunc(number), +number.toFixed()
+];
+
+console.log(multiRound(number));
