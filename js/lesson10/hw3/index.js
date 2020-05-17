a08956849082d287695620888cbbc69e424871ec
ArtemMykholap <56889896+ArtemMykholap@users.noreply.github.com>
2020-05-17 18:29:11 +0300

    1111

--- js/lesson10/hw3/index.js
+++ js/lesson10/hw3/index.js
@@ -1,10 +1,18 @@
-//input:numbers
-//output:array
+//input:number
+//output:number
+
+// задаем требуемые методы
+// умножаем на 10 возведенную в степень n и поделенную на 10 возведенную в степень n
 
 
+let number = 15.711227;
+let n = 3;
 
-let num = 13.137;
+const superRound = (number, n) => [
+    Math.floor(number * 10 ** n) / 10 ** n,
+    Math.round(number * 10 ** n) / 10 ** n,
+    Math.ceil(number * 10 ** n) / 10 ** n,
+    Math.trunc(number * 10 ** n) / 10 ** n, +number.toFixed(n)
+]
 
-const superRound = a => {
-
-}
+console.log(superRound(number, n));
