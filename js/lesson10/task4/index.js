59a2f8b3ad6f7cbe4a181df22631d164516ec846
ArtemMykholap <56889896+ArtemMykholap@users.noreply.github.com>
2020-05-17 14:55:29 +0300

    111

--- js/lesson10/task4/index.js
+++ js/lesson10/task4/index.js
@@ -3,7 +3,7 @@
 
 let arr = [7, 10, -11, 40, -23, -470];
 const getMaxAbsoluteNumber = arr => {
-    if ((!Array.isArray(arr)) || (arr = [])) { return null };
+    if ((!Array.isArray(arr)) || (arr.length === 0)) { return null };
     const absoluteValues = arr.map(num => Math.abs(num));
     const max = Math.max(...absoluteValues);
     return max;
