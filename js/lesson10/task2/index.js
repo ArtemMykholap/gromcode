7de722ce3f4994e0bc46052cb43c3bd37656341b
ArtemMykholap <56889896+ArtemMykholap@users.noreply.github.com>
2020-05-17 13:51:55 +0300

    1111

--- js/lesson10/task2/index.js
+++ js/lesson10/task2/index.js
@@ -7,13 +7,17 @@ const getParsedIntegers = arr => arr.map(elem => Number.parseInt(elem));
 
 const getParsedIntegersV2 = arr => arr.map(elem => parseInt(elem));
 
-const getParsedFloats = arr => arr.filter(elem => Number.parseFloat(elem));
-const getParsedFloatsV2 = arr => arr.filter(elem => parseFloat(elem));
+const getParsedFloats = arr => arr.map(elem => Number.parseFloat(elem));
+const getParsedFloatsV2 = arr => arr.map(elem => parseFloat(elem));
 
 let result = getParsedFloats(arr);
-console.log(result)
+console.log(result);
 let result1 = getParsedFloatsV2(arr);
-console.log(result1)
+console.log(result1);
+let result2 = getParsedIntegers(arr);
+console.log(result2);
+let result3 = getParsedIntegersV2(arr);
+console.log(result3);
 
 
 // let result = getFiniteNumbers(arr);
