88318c78b07f629ce97de5e8dbb8df3484949f97
ArtemMykholap <56889896+ArtemMykholap@users.noreply.github.com>
2020-05-18 09:18:33 +0300

    111

--- js/lesson11/hw2/index.js
+++ js/lesson11/hw2/index.js
@@ -1,6 +1,6 @@
 const text = 'I want to ride my bicycle, bicycle, bicycle';
 const part = 'bi';
-const countOccurences = (text, part) => {
+const countOccurrences = (text, part) => {
     if (part.length === 0) { return null }
 
     return text.split(part).length - 1
