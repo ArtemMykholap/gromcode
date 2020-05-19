4d793d633606e83c715fe352626c329aec086467
ArtemMykholap <56889896+ArtemMykholap@users.noreply.github.com>
2020-05-19 17:18:39 +0300

    1111

--- js/lesson13/hw3/lesson7/hw7/index.test.js
+++ js/lesson13/hw3/lesson7/hw7/index.test.js
@@ -3,20 +3,20 @@
 
 
   it('should return typeof result', () => {
-      const result = filterNames(['John', 'Olivya', 'Oleksandr', 'Emanuel', 'Vanya', 'Nastya'], 'ya');
+      const result = filterNames(['John', 'Olivya', 'Oleksandr', 'Vanya', 'Nastya'], 'ya');
 
       expect(typeof result !== 'string').toEqual(true);
   });
 
 
   it('should return empty array if there are no matching elements ', () => {
-      const result = filterNames(['John', 'Olivya', 'Oleksandr', 'Emanuel', 'Vanya', 'Nastya'], 'der');
+      const result = filterNames(['John', 'Olivya', 'Oleksandr', 'Vanya', 'Nastya'], 'der');
       expect(result).toEqual([]);
   });
 
 
   it('should return length of elements ', () => {
-      const result = filterNames(['John', 'Olivya', 'Oleksandr', 'Emanuel', 'Vanya', 'Nastya'], 'ya');
+      const result = filterNames(['John', 'Olivya', 'Oleksandr', 'Vanya', 'Nastya'], 'ya');
       expect(result[1].length > 5).toEqual(true);
 
   });
