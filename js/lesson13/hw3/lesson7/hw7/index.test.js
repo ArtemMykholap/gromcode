f37c05e5b8585350ad0e80e032cd615fc8437a0a
ArtemMykholap <56889896+ArtemMykholap@users.noreply.github.com>
2020-05-19 17:03:44 +0300

    111

--- js/lesson13/hw3/lesson7/hw7/index.test.js
+++ js/lesson13/hw3/lesson7/hw7/index.test.js
@@ -19,28 +19,3 @@ it('should return length of elements ', () => {
     const result = filterNames(['John', 'Olivya', 'Oleksandr', 'Emanuel', 'Vanya', 'Nastya'], 'ya');
     expect(result[1].length > 5).toEqual(true);
 });
-
-
-
-
-
-
-
-
-
-// import { filterNames } from './index.js'
-
-// it('should return an array', () => {
-//     const result = filterNames(['Jane', 'Violetta', 'Janetta', 'Kate', 'Matador'], 'ta');
-//     const checkType = Array.isArray(result);
-//     expect(checkType).toEqual(true)
-// })
-// it('should check array length', () => {
-//     const result = filterNames(['Jane', 'Violetta', 'Janetta', 'Kate', 'Matador'], 'ta');
-//     expect(result.length).toEqual(3)
-// })
-// it('should check type of array elements', () => {
-//     const result = filterNames(['Jane', 'Violetta', 'Janetta', 'Kate', 'Matador'], 'ta');
-//     const checkTypeOfNum = result.every(i => typeof i === 'string')
-//     expect(checkTypeOfNum).toEqual(true)
-// })
