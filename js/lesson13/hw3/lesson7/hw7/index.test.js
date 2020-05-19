f66c99c044ebd28b2a43c326673ff6ed16a16a60
ArtemMykholap <56889896+ArtemMykholap@users.noreply.github.com>
2020-05-19 17:12:18 +0300

    1111

---
+++ js/lesson13/hw3/lesson7/hw7/index.test.js
@@ -0,0 +1,21 @@
+  import { filterNames } from './index.js'
+
+
+
+  it('should return typeof result', () => {
+      const result = filterNames(['John', 'Olivya', 'Oleksandr', 'Emanuel', 'Vanya', 'Nastya'], 'ya');
+
+      expect(typeof result !== 'string').toEqual(true);
+  });
+
+
+  it('should return empty array if there are no matching elementse ', () => {
+      const result = filterNames(['John', 'Olivya', 'Oleksandr', 'Emanuel', 'Vanya', 'Nastya'], 'der');
+      expect(result).toEqual([]);
+  });
+
+
+  it('should return length of elements ', () => {
+      const result = filterNames(['John', 'Olivya', 'Oleksandr', 'Emanuel', 'Vanya', 'Nastya'], 'ya');
+      expect(result[1].length > 5).toEqual(true);
+  });
