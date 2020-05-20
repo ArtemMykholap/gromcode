69edcbec4730e06ae2d4204190888921a2084479
ArtemMykholap <56889896+ArtemMykholap@users.noreply.github.com>
2020-05-20 20:35:26 +0300

    11111

---
+++ js/lesson15/hw2/index.js
@@ -0,0 +1,46 @@
+//
+
+
+export function createLogger() {
+
+    let memory = [];
+
+    function warn(text) {
+        let warn = {
+            message: text,
+            dateTime: new Date(),
+            'type': 'warn'
+        }
+
+        return memory.push(warn);
+    }
+
+    function error(text) {
+        let error = {
+            message: text,
+            dateTime: new Date(),
+            type: 'error'
+        }
+
+        return memory.push(error);
+    }
+
+    function log(text) {
+        let log = {
+            message: text,
+            dateTime: new Date(),
+            'type': 'log'
+        }
+        return memory.push(log);
+    }
+
+    function getRecords() {
+        return memory;
+    }
+    return {
+        log,
+        getRecords,
+        error,
+        warn,
+    }
+}
---
+++ js/lesson15/hw2/index.test.js
@@ -0,0 +1,16 @@
+import { createLogger } from './index.js'
+
+it('return warn message', () => {
+    let warnTest = createLogger()
+    warnTest.warn('User try to restricted page.');
+    let result = warnTest.getRecords('warn');
+    expect(result[0].message).toEqual('User try to restricted page.');
+});
+
+it('return error message', () => {
+    let errorTest = createLogger();
+    errorTest.error('This message it`s dumb')
+    let result = errorTest.getRecords('error');
+    expect(result[0].message).toEqual('This message it`s dumb');
+
+})
