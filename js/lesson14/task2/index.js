e5618d81e216a604243a2692db9caf71c143ce0f
ArtemMykholap <56889896+ArtemMykholap@users.noreply.github.com>
2020-05-20 11:55:05 +0300

    1111

--- js/lesson14/task2/index.js
+++ js/lesson14/task2/index.js
@@ -5,14 +5,17 @@ const createMessenger = () => {
 
     function sendMessage(name) {
         console.log(`${name}, ${message}. Your ${sender}`);
-    }
+    };
 
     function setMessage(text) {
         message = text;
-    }
+
+        // console.log(text)
+    };
 
     function setSender(text) {
         sender = text;
+        // console.log(text)
     }
     return {
         sendMessage,
@@ -20,22 +23,20 @@ const createMessenger = () => {
         setSender,
     };
 };
-export default createMessenger;
+// export default createMessenger;
 
 
-const messenger = createMessenger();
+const messenger1 = createMessenger();
 const messenger2 = createMessenger();
 const messenger3 = createMessenger();
-messenger.sendMessage('Bob');
-// messenger.setMessage('Good job!');
-// messenger.setSender('Barin')
 
+messenger1.sendMessage('1111')
+messenger1.setMessage('1232131'); //не поменяет ничего,т.к. берет из изначально объявленной .
 
+messenger2.setMessage('Good job!');
+messenger2.sendMessage('Boob');
 
-// messenger2.sendMessage('Tom');
-messenger2.setMessage('Good Job!')
-    // messenger2.setSender('Bond');
-
-
-// messenger3.sendMessage('Arny');
-messenger3.setSender('barin')
+messenger3.setMessage('Good job!');
+messenger3.setSender('Барин');
+messenger3.sendMessage('Bob');
+messenger3.setMessage('Good job!') //ниначто не повлияет,т.к. запрос вівода сообщения происходит раньше настройки мєссєдж.
