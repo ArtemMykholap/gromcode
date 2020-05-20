4b9c82a786175b9b3783ded3b782fbcd128f7818
ArtemMykholap <56889896+ArtemMykholap@users.noreply.github.com>
2020-05-20 09:11:47 +0300

    111111

---
+++ js/lesson14/task2/index.js
@@ -0,0 +1,39 @@
+let message = 'Just learn it';
+let sender = 'Gromcode';
+
+const createMessenger = () => {
+    function sendMessage(name) {
+
+        console.log(`${name}, ${message}. Your ${sender}`);
+
+    }
+
+    function setMessage(text) {
+        message = text;
+    }
+
+    function setSender(text) {
+        sender = text;
+    }
+    return {
+        sendMessage,
+        setMessage,
+        setSender,
+    };
+};
+
+
+// const messenger = createMessenger();
+// messenger.sendMessage('Bob');
+// messenger.setMessage('Good job!');
+// messenger.setSender('Barin')
+
+
+// const messenger2 = createMessenger();
+// messenger2.sendMessage('Tom');
+// messenger2.setMessage('Good Job!')
+// messenger2.setSender('Bond')
+
+// const messenger3 = createMessenger();
+// messenger3.sendMessage('Arny')
+// messenger3.setSender('barin')
