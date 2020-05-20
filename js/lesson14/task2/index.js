9c2323e0618349be1426c570efaacacf30849ae6
ArtemMykholap <56889896+ArtemMykholap@users.noreply.github.com>
2020-05-20 11:08:29 +0300

    1111

--- js/lesson14/task2/index.js
+++ js/lesson14/task2/index.js
@@ -26,16 +26,16 @@ export default createMessenger;
 const messenger = createMessenger();
 const messenger2 = createMessenger();
 const messenger3 = createMessenger();
-// messenger.sendMessage('Bob');
+messenger.sendMessage('Bob');
 // messenger.setMessage('Good job!');
 // messenger.setSender('Barin')
 
 
 
 // messenger2.sendMessage('Tom');
-// messenger2.setMessage('Good Job!')
-// messenger2.setSender('Bond');
+messenger2.setMessage('Good Job!')
+    // messenger2.setSender('Bond');
 
 
 // messenger3.sendMessage('Arny');
-//   messenger3.setSender('barin')
+messenger3.setSender('barin')
