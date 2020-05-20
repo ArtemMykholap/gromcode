81ed9f8e8dffd7b40791bf54ba8734bd57a2b27e
ArtemMykholap <56889896+ArtemMykholap@users.noreply.github.com>
2020-05-20 13:28:42 +0300

    11111

---
+++ js/lesson14/hw1/index.js
@@ -0,0 +1,43 @@
+// input:number;
+// output:number;
+
+// Создаем функции соответсвющие их назначениямж
+// приводим их к объекту, в следствии чего можем ими пользоваться,как методами.
+
+
+let memory = 0;
+
+
+function calc() {
+
+
+    function add(number) {
+        return memory += number;
+    }
+
+    function decrease(number) {
+        return memory -= number;
+    }
+
+    function reset(number) {
+        return memory = 0;
+    }
+
+    function getMemo() {
+        return memory;
+    }
+    return {
+        add,
+        decrease,
+        reset,
+        getMemo,
+    }
+
+}
+// let total = calc();
+// total.add(5);
+
+// let totalDec=calc();
+// totalDec.decrease(3);
+
+export { add, decrease, reset, getMemo }
