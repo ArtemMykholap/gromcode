7aa933cef4c2de2a32d298e7e92095c7d305f3a0
ArtemMykholap <56889896+ArtemMykholap@users.noreply.github.com>
2020-05-20 12:07:10 +0300

    111

--- js/lesson14/task2/index.js
+++ js/lesson14/task2/index.js
@@ -4,7 +4,7 @@ const createMessenger = () => {
     let sender = 'Gromcode';
 
     function sendMessage(name) {
-        console.log(`${name}, ${message}. Your ${sender}`);
+        console.log(`${name}, ${message}\! Your ${sender}`);
     };
 
     function setMessage(text) {
@@ -30,13 +30,13 @@ const messenger1 = createMessenger();
 const messenger2 = createMessenger();
 const messenger3 = createMessenger();
 
-messenger1.sendMessage('1111')
-messenger1.setMessage('1232131'); //не поменяет ничего,т.к. берет из изначально объявленной .
+messenger1.sendMessage('Bob');
+// messenger1.setMessage('1232131'); //не поменяет ничего,т.к. берет из изначально объявленной .
 
-messenger2.setMessage('Good job!');
-messenger2.sendMessage('Boob');
+messenger2.setMessage('Good job');
+messenger2.sendMessage('Bob');
 
-messenger3.setMessage('Good job!');
-messenger3.setSender('Барин');
+
+messenger3.setSender('Gromcode');
 messenger3.sendMessage('Bob');
-messenger3.setMessage('Good job!') //ниначто не повлияет,т.к. запрос вівода сообщения происходит раньше настройки мєссєдж.
+// messenger3.setMessage('Good job!') //ниначто не повлияет,т.к. запрос вівода сообщения происходит раньше настройки мєссєдж.
