32980bd41302e6fed156d89bd599805ea4068a52
ArtemMykholap <56889896+ArtemMykholap@users.noreply.github.com>
2020-05-22 14:51:44 +0300

    defer

---
+++ js/lesson17/task3/index.js
@@ -0,0 +1,21 @@
+// defer(func,ms)=> Function
+
+function defer(func, ms) {
+    return function() {
+        setTimeout(() => func.apply(this, arguments), ms);
+    }
+}
+const user = {
+    name: 'Tom',
+    sayHi() {
+        console.log(`Hi, I'am ${this.name}!`)
+    }
+}
+
+
+const deferredHi = defer(user.sayHi, 1000);
+
+deferredHi.call({ name: 'Bob' })
+const sayHi = () => {
+    console.log('Hi');
+}
