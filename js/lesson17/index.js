3eabbf09033d76be5618dacbb46d01708ae8c0e4
ArtemMykholap <56889896+ArtemMykholap@users.noreply.github.com>
2020-05-22 13:29:03 +0300

    return

--- js/lesson17/index.js
+++ js/lesson17/index.js
@@ -2,7 +2,7 @@
      firstName: 'John',
      lastName: 'Doe',
      getFullName() {
-         console.log(`${this.firstName} ${this.lastName}`);
+         return (`${this.firstName} ${this.lastName}`);
      }
  };
  export { user }
