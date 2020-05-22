875e93f8fb61a8aa29d952dd6a7f61cbb17555b5
ArtemMykholap <56889896+ArtemMykholap@users.noreply.github.com>
2020-05-22 12:29:02 +0300

    numb

--- js/lesson16/hw1/index.js
+++ js/lesson16/hw1/index.js
@@ -6,10 +6,10 @@
 
 export function createArrayOfFunctions(num) {
     let arr = []
-    if (typeof(num) !== 'number') {
-        return null;
-    } else if (num === undefined) {
+    if (num === undefined) {
         return arr;
+    } else if (typeof(num) !== 'number') {
+        return null;
     } else {
         for (let i = 0; i < num; i++) {
             arr[i] = function() {
