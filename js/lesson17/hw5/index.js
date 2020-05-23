7efb196a70f75e4708b31df602646f554f8700f1
ArtemMykholap <56889896+ArtemMykholap@users.noreply.github.com>
2020-05-24 00:31:21 +0300

    timer

---
+++ js/lesson17/hw5/index.js
@@ -0,0 +1,30 @@
+let timer = {
+    secondsPassed: 0,
+    minsPassed: 0,
+    timerId = 0,
+    startTimer() {
+        this.timerId = setInterval(() => {
+            this.secondsPassed++;
+            if (this.secondsPassed === 60) { //if seconds reach 60-nullfy and plus one minut
+                this.secondsPassed = 0;
+                this.minsPassed++;
+            }
+        }, 1000); //interval seconds
+    },
+    getTime() {
+        return `${this.minsPassed} : ${this.secondsPassed < 10 ? '0' + this.secondsPassed : this.secondsPassed}`;
+        // if (this.secondsPassed < 10) {
+        //     console.log('0' + this.secondsPassed);
+        // } else {
+        //     console.log(this.secondsPassed);
+        // }
+
+    },
+    stopTimer() {
+        clearInterval(this.timerId);
+    },
+    resetTimer() { //nullfy
+        this.secondsPassed = 0;
+        this.minsPassed = 0;
+    },
+}
