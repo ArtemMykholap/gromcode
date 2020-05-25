892eb61925349b1a2b6796a971c01ada03eaaa0a
ArtemMykholap <56889896+ArtemMykholap@users.noreply.github.com>
2020-05-25 11:57:19 +0300

    bind

--- js/lesson17/hw6/index.js
+++ js/lesson17/hw6/index.js
@@ -14,6 +14,35 @@ export function calculator(a, b) {
 }
 
 
+
+
+function multiplier(a, b) {
+
+    const functMult = calculator.bind({ operation: '*' });
+    return functMult(a, b)
+}
+// console.log(multiplier(3, 7))
+
+
+
+function summator(a, b) {
+    const funcSum = calculator.bind({ operation: '+' });
+    return funcSum(a, b)
+}
+// console.log(summator(3, 7))
+
+
+
+function twice(a) {
+    //  const funcTwice=calculator.bind({operation:'*'});
+    return a * 2
+}
+// console.log(twice(3))
+
+
+
+
+
 /*
  * Ф-ция multiplier должна быть создана на основе calculator
  * с использования .bind
