27f618d878f475056c59d7677594f82e71851a57
ArtemMykholap <56889896+ArtemMykholap@users.noreply.github.com>
2020-05-25 15:49:06 +0300

    jon

--- js/lesson18/task1/index.js
+++ js/lesson18/task1/index.js
@@ -1,16 +1,16 @@
 export const event = {
     guests: [{
             name: 'Tom',
-            email: 't@gmail.com',
+            email: 'example@server.com',
             age: 17,
         },
         {
-            name: 'Bob',
-            email: 'b@gmail.com',
+            name: 'John',
+            email: 'example@server.com',
             age: 18,
         }
     ],
-    message: ' Welcome to the party!',
+    message: ' Welcome to the party',
     getInvitations() {
         return this.guests
             .filter(({ age }) => age >= 18)
