2737cdb14c498a3bcc2e85915a796ccc583d8446
ArtemMykholap <56889896+ArtemMykholap@users.noreply.github.com>
2020-05-25 16:38:31 +0300

    pseudo arr

---
+++ js/lesson18/task3/index.js
@@ -0,0 +1,7 @@
+export function sumOfSquares() {
+    return [].reduce.apply(arguments, [(acc, elem) => {
+        return acc + (elem * elem);
+    }, 0])
+}
+
+// console.log(sumOfSquares(1, 2, 3, 4));
