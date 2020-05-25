a4303710039f68ed033f589585e0ee5d79c486c9
ArtemMykholap <56889896+ArtemMykholap@users.noreply.github.com>
2020-05-25 10:20:30 +0300

    1111

---
+++ js/stand-up/psevdo array/index.js
@@ -0,0 +1,30 @@
+// let arr = [5, 6, 7, 10];
+// console.log(...arr);
+
+
+// for(let i=0; i<arr.length;i++) {
+//   console.log(arr[i]);
+// }
+
+
+// function sum(a,b, ...args){
+//   console.log(a);
+//   console.log(b);
+//   console.log(args);
+// }
+
+//task input number.nember..
+//outputL number
+// function multiply(...numbers){
+//             return numbers.reduce((acc, el)=>acc*el)
+// }
+// console.log(multiply(123,123,123));
+
+
+function multiply2() {
+    let args = arguments;
+    console.log(args);
+
+    return [...args].reduce((acc, el) => acc * el)
+}
+console.log(multiply2(123, 123, 123));
