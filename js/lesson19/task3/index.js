18a3ebb05f5cb3542ed4ff4ed144a3b9662c8543
ArtemMykholap <56889896+ArtemMykholap@users.noreply.github.com>
2020-05-27 14:04:28 +0300

    protoype

---
+++ js/lesson19/task3/index.js
@@ -0,0 +1,36 @@
+//user
+
+function User(name, age) {
+    this.name = name;
+    this.age = age;
+}
+User.prototype.sayHi = function() {
+    console.log(`Hi,I am ${this.name}`)
+}
+User.prototype.requestNewPhoto = function() {
+    console.log(`New photo request was for ${this.name}`)
+}
+User.prototype.setAge = function(num) {
+    if (num < 0) return false;
+    this.age = num;
+    if (num >= 25) {
+        console.log(`New photo request was for ${this.name}`)
+    }
+    return this.age;
+
+}
+
+
+const user1 = new User('John', );
+const user2 = new User('Bob', 27);
+
+// console.log(user1);
+// console.log(user2);
+// user1.requestNewPhoto()
+// user1.setAge(-22);
+//  console.log(user1);
+
+// user1.sayHi()
+// user2.sayHi()
+
+// console.log(user1.sayHi===user2.sayHi)
