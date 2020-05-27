09d91b9e687ea66c6c3fa58065e31ed5842d1f0e
ArtemMykholap <56889896+ArtemMykholap@users.noreply.github.com>
2020-05-27 14:13:41 +0300

    export

--- js/lesson19/task3/index.js
+++ js/lesson19/task3/index.js
@@ -1,33 +1,32 @@
 //user
-
 export function User(name, age) {
     this.name = name;
     this.age = age;
 }
 User.prototype.sayHi = function() {
-    console.log(`Hi,I am ${this.name}`)
+    console.log(`Hi, I am ${this.name}`)
 }
 User.prototype.requestNewPhoto = function() {
-    console.log(`New photo request was for ${this.name}`)
+    console.log(`New photo request was sent for ${this.name}`)
 }
 User.prototype.setAge = function(num) {
     if (num < 0) return false;
     this.age = num;
     if (num >= 25) {
-        console.log(`New photo request was for ${this.name}`)
+        this.requestNewPhoto()
     }
     return this.age;
 
 }
 
 
-const user1 = new User('John', );
+const user1 = new User('John', 11);
 const user2 = new User('Bob', 27);
 
 // console.log(user1);
 // console.log(user2);
 // user1.requestNewPhoto()
-// user1.setAge(-22);
+user1.setAge(25);
 //  console.log(user1);
 
 // user1.sayHi()
