6d4d95e19f2524baec772ace3608e4b4b9861b64
ArtemMykholap <56889896+ArtemMykholap@users.noreply.github.com>
2020-05-27 12:57:47 +0300

    let

--- js/lesson19/task2/index.js
+++ js/lesson19/task2/index.js
@@ -8,9 +8,22 @@ const vehicle = {
     }
 };
 
+
+const user = {
+    name: 'A',
+    nick: 'B',
+    startMachine() {
+        console.log(`${this.name} lifting anchor up`);
+        ship.move();
+
+    },
+}
+
+
 const ship = {
     name: 'Argo',
     lastName: 'Argovich',
+    fiName: 'Argovich',
     startMachine() {
         console.log(`${this.name} lifting anchor up`);
         ship.move();
@@ -23,12 +36,49 @@ const ship = {
     __proto__: vehicle,
 }
 
-let keysArr = [];
-for (let prop in ship) {
-    if (ship.hasOwnProperty(prop) && typeof ship[prop] !== 'function') {
-        keysArr.push(prop)
+
+
+
+function getOwnProps(ship) {
+    let keysArr = [];
+    for (let prop in ship) {
+        if (ship.hasOwnProperty(prop) && typeof ship[prop] !== 'function') {
+            keysArr.push(prop);
+
+        }
+
     }
+    return keysArr
 }
+
+
+
+// function getOwnProps(ship) {
+//     // console.log(ship)
+//     let keysArr = [];
+//     for (let prop in ship) {
+//         // console.log(keysArr)
+//         if (ship.hasOwnProperty(prop) && typeof ship[prop] !== 'function') {
+//             //  console.log(keysArr)
+//             keysArr.push(prop);
+
+
+//             //  console.log(keysArr)
+
+//         }
+
+//     }
+//     return keysArr
+// }
+
+
+// console.log(getOwnProps(user))
+
+
+
+
+//  console.log(keysArr)
+
 
 
 //  console.log(keysArr)
