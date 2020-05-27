b6c84e1e134df53ba50392761bece9fa5d3f42ce
ArtemMykholap <56889896+ArtemMykholap@users.noreply.github.com>
2020-05-27 15:36:51 +0300

    exxx

--- js/lesson20/task1/index.js
+++ js/lesson20/task1/index.js
@@ -1,5 +1,4 @@
 //classes
-//classes
 class User {
     constructor(name, age) {
         this.name = name;
@@ -17,37 +16,19 @@ class User {
         if (num >= 25) {
             this.requestNewPhoto()
         }
+        return num;
 
 
     }
     static createEmpty() {
-        this.name = [];
-        this.age = null;
+        let newUser = new User('', null)
+        return newUser;
     }
 }
+// const user1= new User('Tom',17);
+console.log(User.createEmpty());
+
+
 export { User }
-
-
-// function User(name, age) {
-//     this.name = name;
-//     this.age = age;
-// }
-// User.prototype.sayHi = function() {
-//     console.log(`Hi, I am ${this.name}`)
-// }
-// User.prototype.requestNewPhoto = function() {
-//     console.log(`New photo request was sent for ${this.name}`)
-// }
-// User.prototype.setAge = function(num) {
-//     if (num < 0) { return false };
-//     this.age = num;
-//     if (num >= 25) {
-//         this.requestNewPhoto()
-//     }
-//     return num;
-
-// }
-
-
-// const user1 = new User('John', 11);
-// const user2 = new User('Bob', 27);
+// console.log(user1.createEmpty=[])
+// createEmpty()
