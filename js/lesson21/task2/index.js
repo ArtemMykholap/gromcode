c553094023ac5aaa9357820898a0ee4cb4de5fc2
ArtemMykholap <56889896+ArtemMykholap@users.noreply.github.com>
2020-05-29 17:13:45 +0300

    query

---
+++ js/lesson21/task2/index.js
@@ -0,0 +1,13 @@
+const getTitleElement = () => {
+        export const titleElem = document.querySelector('.title'); //ищем селектор по классу
+        console.dir(titleElem); //выводим в консоль
+        return titleElem;
+    }
+    // getTitleElement()
+
+export const getInputElement = () => {
+        const inputElem = document.querySelector('input[type="text"]'); //ищем селектор по типу
+        console.dir(inputElem);
+        return inputElem;
+    }
+    // getInputElement()
