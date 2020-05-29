7c6591d895c1856402c024f4a79abda95d11ce52
ArtemMykholap <56889896+ArtemMykholap@users.noreply.github.com>
2020-05-29 19:10:45 +0300

    text

---
+++ js/lesson21/hw3/index.js
@@ -0,0 +1,30 @@
+const getTitle = () => {
+        const elem = document.querySelector('.title');
+        return elem.textContent;
+    }
+    // getTitle()
+
+
+const getDescription = () => {
+        const elem = document.querySelector('.about');
+        return elem.innerText;
+    }
+    // getDescription()
+
+const getPlans = () => {
+        const elem = document.querySelector('.plans');
+        return elem.innerHTML;
+    }
+    // getPlans()
+
+const getGoal = () => {
+        const elem = document.querySelector('.goal');
+        return elem.outerHTML;
+    }
+    // getGoal()
+export {
+    getGoal,
+    getPlans,
+    getDescription,
+    getTitle
+}
