f2842e35606ff8e969aa29d6ff56491527543e89
ArtemMykholap <56889896+ArtemMykholap@users.noreply.github.com>
2020-05-29 17:15:53 +0300

    export

--- js/lesson21/task2/index.js
+++ js/lesson21/task2/index.js
@@ -1,13 +1,15 @@
-export const getTitleElement = () => {
-        export const titleElem = document.querySelector('.title'); //ищем селектор по классу
-        console.dir(titleElem); //выводим в консоль
-        return titleElem;
-    }
-    // getTitleElement()
+ const getTitleElement = () => {
+         export const titleElem = document.querySelector('.title'); //ищем селектор по классу
+         console.dir(titleElem); //выводим в консоль
+         return titleElem;
+     }
+     // getTitleElement()
 
-export const getInputElement = () => {
-        const inputElem = document.querySelector('input[type="text"]'); //ищем селектор по типу
-        console.dir(inputElem);
-        return inputElem;
-    }
-    // getInputElement()
+ const getInputElement = () => {
+         const inputElem = document.querySelector('input[type="text"]'); //ищем селектор по типу
+         console.dir(inputElem);
+         return inputElem;
+     }
+     // getInputElement()
+
+ export { getInputElement, getTitleElement }
