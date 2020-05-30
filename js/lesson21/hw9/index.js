70409e95137244d9abb78ec9cdd7996e3837b022
ArtemMykholap <56889896+ArtemMykholap@users.noreply.github.com>
2020-05-30 09:47:11 +0300

    export

--- js/lesson21/hw9/index.js
+++ js/lesson21/hw9/index.js
@@ -1,10 +1,10 @@
- export const finishForm = () => {
-         const changeAttribute = document.querySelector('input[type="text"]')
-         changeAttribute.type = 'password'; //first type for change attribute
-         const finishList = document.createElement('input');
-         finishList.setAttribute('type', 'text'); //second type for change and add attributes
-         finishList.setAttribute('name', 'login');
-         const elementInput = document.querySelector('.login-form');
-         elementInput.append(newInput);
-     }
-     // finishForm()
+export const finishForm = () => {
+        const changeAttribute = document.querySelector('input[type="text"]')
+        changeAttribute.type = 'password'; //first type for change attribute
+        const finishList = document.createElement('input');
+        finishList.setAttribute('type', 'text'); //second type for change and add attributes
+        finishList.setAttribute('name', 'login');
+        const elementInput = document.querySelector('.login-form');
+        elementInput.prepend(finishList);
+    }
+    // finishForm()
