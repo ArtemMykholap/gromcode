735c3a43d6eb6799029b207620ff233e8a167258
ArtemMykholap <56889896+ArtemMykholap@users.noreply.github.com>
2020-06-01 16:10:00 +0300

    input

---
+++ js/lesson22/hw4/index.js
@@ -0,0 +1,6 @@
+const textElem = document.querySelector('.search__btn');
+
+const textElem2 = document.querySelector('.search__input')
+textElem.addEventListener('click', () => {
+    console.log(textElem2.value)
+})
