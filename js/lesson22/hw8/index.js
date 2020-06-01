c838e80798283bf7abbdf03a7b9336a32a311d4b
ArtemMykholap <56889896+ArtemMykholap@users.noreply.github.com>
2020-06-01 17:36:00 +0300

    event

--- js/lesson22/hw8/index.js
+++ js/lesson22/hw8/index.js
@@ -1,5 +1,5 @@
 const elem = document.querySelector('.task-status');
 
-elem.addEventListener('change', () => {
+elem.addEventListener('change', (event) => {
     console.log(event.target.checked);
 });
