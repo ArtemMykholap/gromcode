86c385e11eb40828e7bf6c2367326109b4cb1fd7
ArtemMykholap <56889896+ArtemMykholap@users.noreply.github.com>
2020-06-07 12:31:11 +0300

    date

---
+++ js/lesson24/task1/index.js
@@ -0,0 +1,11 @@
+const weekDays = ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'St']
+
+
+const DayOfWeek = (date, days) => {
+    const day = new Date(date).getDay();
+    const dateInFuture = new Date(date).setDate(day + days);
+    return weekDays[new Date(dateInFuture).getDay()];
+};
+const result = DayOfWeek(new Date(2019, 0, 1), 14)
+
+console.log(result);
