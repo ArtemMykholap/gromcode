9c57891943bc982709c1fd24d016f799cdb9557a
ArtemMykholap <56889896+ArtemMykholap@users.noreply.github.com>
2020-06-08 13:50:17 +0300

    push

--- js/lesson24/hw1/index.js
+++ js/lesson24/hw1/index.js
@@ -7,31 +7,27 @@
 // let endDate = new Date('1/12/2019/ 04:03:06');
 
 
+let startDate = new Date('1/11/2018/ 01:01:01');
+let endDate = new Date('1/12/2019/ 04:03:06');
+
+
 function getDiff(startDate, endDate) {
-    const msInDay = 1000 * 60 * 60 * 24;
+    const msInMinut = 1000 * 60;
+    const msInHour = msInMinut * 60;
+    const msInDay = msInHour * 24;
     const modulTime = Math.abs(startDate.getTime() - endDate.getTime());
 
-    const howDaysFloor = Math.floor(modulTime / msInDay);
-    const howHoursMs = modulTime - (howDaysFloor * msInDay);
-    const howHoursFloor = Math.floor(howHoursMs / (msInDay / 24));
-    const howMinutesMs = modulTime - (howHoursFloor * msInDay / 24 + howDaysFloor * msInDay);
-    const howMinutesFloor = Math.floor(howMinutesMs / (1000 * 60));
-    const howSecMs = modulTime - (howMinutesFloor * (1000 * 60) + howDaysFloor * msInDay + howHoursFloor * (1000 * 60 * 60))
-    const howSecFloor = Math.floor(howSecMs / 1000);
+    const howDays = Math.floor(modulTime / msInDay);
+    const restAfterDays = modulTime % msInDay;
+    const howHours = Math.floor(restAfterDays / msInHour);
+    const restAfterHours = modulTime % msInHour;
+    const howMinutes = Math.floor(restAfterHours / msInMinut);
+    const restAfterMinutes = modulTime % msInMinut;
+    const howSec = Math.floor(restAfterMinutes / 1000);
 
-    // console.log(modulTime);
-    // console.log(howDaysFloor)
-    // console.log(howHoursMs)
-    // console.log(howHoursFloor)
-    // console.log(howMinutesMs)
-    // console.log(howMinutesFloor)
-    // console.log(howSecFloor)
-    return `${howDaysFloor}d ${howHoursFloor}h ${howMinutesFloor}m ${howSecFloor}s `
+
+    return `${howDays}d ${howHours}h ${howMinutes}m ${howSec}s `
 }
 
-
-// console.log(getDiff(startDate, endDate))
-
-// console.log(startDate)
-// console.log(endDate)
-export { getDiff };
+// console.log (getDiff(startDate, endDate))
+export { getDiff }
