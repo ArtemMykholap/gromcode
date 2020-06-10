dcca7522e9c519ba90abaadb190228ce79f8ef30
ArtemMykholap <56889896+ArtemMykholap@users.noreply.github.com>
2020-06-10 18:15:27 +0300

    json

--- js/lesson27/task1/index.js
+++ js/lesson27/task1/index.js
@@ -1,15 +1,21 @@
 localStorage.clear()
 localStorage.setItem('hobbies', JSON.stringify({ name: 'Tom' }));
-localStorage.setItem('name', JSON.stringify('Tom'));
+localStorage.setItem('name', 'Tom');
 localStorage.setItem('age', JSON.stringify(17));
 // localStorage.getItem('name')
 
-export const getLocalStorageData = () => {
+const getLocalStorageData = () => {
     return Object.entries(localStorage)
         .reduce((acc, [key, value]) => {
+            let newValue;
+            try {
+                newValue = JSON.parse(value)
+            } catch (e) {
+                newValue = value
+            }
             return {
                 ...acc,
-                [key]: JSON.parse(value),
+                [key]: newValue,
             };
         }, {});
 }
