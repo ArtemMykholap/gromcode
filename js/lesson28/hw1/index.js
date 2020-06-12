e66dc1459f9728d1c1a8b7cbd8efaca6fdea9e95
ArtemMykholap <56889896+ArtemMykholap@users.noreply.github.com>
2020-06-12 18:44:43 +0300

    karry

---
+++ js/lesson28/hw1/index.js
@@ -0,0 +1,44 @@
+// input:dateObject
+// output:dateObject with method 
+
+
+
+
+function shmoment(date) {
+    let newDate = new Date(date);
+    const reTime = {
+        add(key, value) {
+            let upDate = {
+                years: (value) => (date.setFullYear(date.getFullYear() + value)),
+                month: (value) => (date.setMonth(date.getMonth() + value)),
+                days: (value) => (date.setDate(date.getDate() + value)),
+                hours: (value) => (date.setHours(date.getHours() + value)),
+                minutes: (value) => (date.setMinutes(date.getMinutes() + value)),
+                seconds: (value) => (date.setSeconds(date.getSeconds() + value)),
+                milliseconds: (value) => (date.setMilliseconds(date.getMilliseconds() + value)),
+            }
+            newDate = new Date(upDate[key](value));
+            return reTime
+        },
+        subtract(key, value) {
+            let upDate = {
+                years: (value) => (date.setFullYear(date.getFullYear() - value)),
+                month: (value) => (date.setMonth(date.getMonth() - value)),
+                days: (value) => (date.setDate(date.getDate() - value)),
+                hours: (value) => (date.setHours(date.getHours() - value)),
+                minutes: (value) => (date.setMinutes(date.getMinutes() - value)),
+                seconds: (value) => (date.setSeconds(date.getSeconds() - value)),
+                milliseconds: (value) => (date.setMilliseconds(date.getMilliseconds() - value)),
+            }
+            newDate = new Date(upDate[key](value));
+            return reTime
+        },
+        result() {
+            return newDate
+        }
+
+    }
+
+    return reTime
+}
+console.log(shmoment(new Date()).subtract('years', 3).result());
