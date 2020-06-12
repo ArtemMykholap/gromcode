f1d8ada19a03c9f3479716fc916c2669a13664d9
ArtemMykholap <56889896+ArtemMykholap@users.noreply.github.com>
2020-06-12 15:19:32 +0300

    calc

--- js/lesson28/task1/index.js
+++ js/lesson28/task1/index.js
@@ -1,32 +1,32 @@
 export const calc = initValue => {
-    let result = initValue;
+        let result = initValue;
 
-    const calculator = {
-        add(value) {
-            result += value;
-            return calculator;
-        },
-        mult(value) {
-            result *= value;
-            return this;
-        },
-        substract(value) {
-            result -= value;
-            return this;
-        },
-        div(value) {
-            result /= value;
-            return this;
-        },
-        result() {
+        const calculator = {
+            add(value) {
+                result += value;
+                return this;
+            },
+            mult(value) {
+                result *= value;
+                return this;
+            },
+            subtract(value) {
+                result -= value;
+                return this;
+            },
+            div(value) {
+                result /= value;
+                return this;
+            },
+            result() {
 
-            return result;
-        },
+                return result;
+            },
 
-    };
+        };
 
-    return calculator;
-}
-const result = calc(3).add(2).mult(4).div(10).substract(5).result();
+        return calculator;
+    }
+    // const result = calc(3).add(2).mult(4).div(10).substract(5).result();
 
-console.log(result);
+// console.log(result);
