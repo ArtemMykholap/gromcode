c98916fc1fa3b0d4d39b877e88367f0f3326ac92
ArtemMykholap <56889896+ArtemMykholap@users.noreply.github.com>
2020-06-14 14:43:52 +0300

    callback

---
+++ js/lesson29/hw1/index.js
@@ -0,0 +1,22 @@
+// const randomInterval = (min, max) =>
+//     Math.floor(min + Math.random() * (max + 1 - min))
+// console.log(randomInterval(1, 3) * 1000);
+
+
+
+const requestUserData = (userId, callback) => {
+    // console.log(callback)
+    // console.log(userId)
+    if (userId === 'broken') {
+        callback(null, 'Failed to load user data');
+        return;
+    }
+    const randomInterval = (min, max) =>
+        Math.floor(min + Math.random() * (max + 1 - min));
+    const random = randomInterval(1, 3) * 1000;
+
+    setTimeout(() => callback({
+        userId: userId,
+        email: `${userId}@examle.com`,
+    }), random)
+};
