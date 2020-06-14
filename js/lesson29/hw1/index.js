4290cc564cea38844cf2d2ccd2e23d472bca501f
ArtemMykholap <56889896+ArtemMykholap@users.noreply.github.com>
2020-06-14 15:10:39 +0300

    export

--- js/lesson29/hw1/index.js
+++ js/lesson29/hw1/index.js
@@ -3,7 +3,7 @@
 // console.log(randomInterval(1, 3) * 1000);
 
 
-const requestUserData = (userId, callback) => {
+export const requestUserData = (userId, callback) => {
     const randomInterval = (min, max) =>
         Math.floor(min + Math.random() * (max + 1 - min));
     const random = randomInterval(1, 3) * 1000;
