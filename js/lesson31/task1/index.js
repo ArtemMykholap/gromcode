316d64e37ed11476070be0ca09a2de4466e8be5f
ArtemMykholap <56889896+ArtemMykholap@users.noreply.github.com>
2020-06-16 11:03:47 +0300

    reject_resolve

---
+++ js/lesson31/task1/index.js
@@ -0,0 +1,23 @@
+export const requestUserData = userId => {
+    const request = new Promise((resolve, reject) => {
+        if (userId === 'broken') {
+            setTimeout(() => {
+                reject(new Error('User not found'))
+            }, 500);
+        } else {
+            setTimeout(() => {
+                resolve({
+                    name: 'John',
+                    age: 17,
+                    email: `${userId}@example.com`,
+                    userId,
+                });
+            }, 1000);
+        }
+
+
+    });
+    return request
+};
+requestUserData('broken')
+    .catch(data => console.log(data))
