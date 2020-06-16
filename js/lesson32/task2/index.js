e22a93682ef5f677de2f04283bab8e9da1fe30d8
ArtemMykholap <56889896+ArtemMykholap@users.noreply.github.com>
2020-06-16 18:23:51 +0300

    server

---
+++ js/lesson32/task2/index.js
@@ -0,0 +1,32 @@
+const getRandomNumber = (from, to) =>
+    from + Math.random() * (to - from);
+
+const request = url => new Promise(resolve => {
+    const randomDelay = getRandomNumber(1000, 3000);
+    setTimeout(() => {
+        resolve({
+            userData: {
+                name: 'Tom',
+                age: 17,
+            },
+            source: url
+        })
+    }, randomDelay);
+});
+
+const servers = [
+    'https://server.com/us/userId',
+    'https://server.com/eu/userId',
+    'https://server.com/au/userId',
+]
+
+export const getUserAsap = userId => {
+    const userUrls = servers
+        .map(serverUrl => `${serverUrl}/1${userId}`);
+
+    const requests = userUrls.map(userUrl => request(userUrl));
+    return Promise.race(requests);
+}
+
+getUserAsap('user-id-1')
+    .then(res => console.log(res))
