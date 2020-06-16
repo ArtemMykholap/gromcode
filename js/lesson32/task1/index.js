e251a68670a9ff33b4f4aa5bf73a2ddb1882f9e0
ArtemMykholap <56889896+ArtemMykholap@users.noreply.github.com>
2020-06-16 17:28:58 +0300

    async

--- js/lesson32/task1/index.js
+++ js/lesson32/task1/index.js
@@ -1,7 +1,7 @@
 const getValueWithDelay = (value, delay) =>
     new Promise(resolve => {
         setTimeout(() => {
-            console.log(value)
+            // console.log(value)
             resolve(value);
         }, delay)
     });
@@ -21,8 +21,8 @@ const getSum = numbers =>
 export const asyncSum = (...asyncNumbers) => {
     return Promise.all(asyncNumbers)
         .then(numbers => getSum(numbers))
-        .catch(() => Promise.reject(new Erroro('Can\'t calculate')))
+        .catch(() => Promise.reject(new Error('Can\'t calculate')))
 };
 
-asyncSum(asyncNum1, reject(new Error('err')), asyncNum3)
+asyncSum(asyncNum1, Promise.reject(new Error('err')), asyncNum3)
     .then(result => console.log(result))
