61b17f3d274fd58bf81f1ada4f255ad751d7aad6
ArtemMykholap <56889896+ArtemMykholap@users.noreply.github.com>
2020-06-22 14:04:45 +0300

    methods

--- js/lesson33/hw2/index.js
+++ js/lesson33/hw2/index.js
@@ -1,20 +1,3 @@
-// const baseUrl = 'https://5e5cf5eb97d2ea0014796f01.mockapi.io/api/v1/tasks';
-// export const getTasksList = () =>
-//     fetch(baseUrl).then(response => response.json())
-//     // .then(result => console.log(result))
-
-// // getTasksList()
-
-// export const getTaskById = id =>
-//     fetch(`${baseUrl}/${id}`)
-//     .then(response => response.json())
-//     // .then(result => console.log(result))
-
-// // getTaskById(10)
-
-
-
-
 const commitsUrl = 'https://developer.github.com/v3/repos/commits/#list-commits-on-a-repository';
 
 const getCommits = () =>
---
+++ js/lesson34/task2/index.js
@@ -0,0 +1,38 @@
+const baseUrl = 'https://5ef08099ad6d71001617a47b.mockapi.io/api/v1/users';
+export const getUsersList = () =>
+    fetch(baseUrl).then(response => response.json())
+
+
+
+export const getTaskById = userId =>
+    fetch(`${baseUrl}/${userId}`)
+    .then(response => response.json())
+
+export const createUser = obj => {
+    return fetch(baseUrl, {
+        method: 'POST',
+        header: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(obj),
+
+    })
+};
+
+
+export const updateUser = (obj, userId) => {
+    return fetch(`${baseUrl}/${userId}`, {
+        method: 'PUT',
+        header: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(obj),
+
+    })
+};
+
+export const deleteUser = id => {
+    return fetch(`${baseUrl}/${userId}`, {
+        method: 'DELETE',
+    })
+}
