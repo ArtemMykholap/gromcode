7e5f422b34f636e00721c373b08bc71650ac6a9b
ArtemMykholap <56889896+ArtemMykholap@users.noreply.github.com>
2020-06-23 01:01:00 +0300

    aa

--- js/lesson34/hw2/index.js
+++ js/lesson34/hw2/index.js
@@ -16,9 +16,9 @@ loginFormElem.addEventListener("submit", function(event) {
     // console.log(loginFormElem)
 
     event.preventDefault();
-
-
-    const formData = Object.assign({}, [...new FormData(loginFormElem)].flat(1))
+    // const formData = Object.assign({}, [...new FormData(loginFormElem)].flat(1))
+    const formData = [...new FormData(loginFormElem)]
+        .reduce((acc, [input, value]) => ({...acc, [input]: value }), {});
 
     console.log(formData)
 
