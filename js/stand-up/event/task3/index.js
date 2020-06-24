d71a08a17de21809126c93aac906482402dac1a8
ArtemMykholap <56889896+ArtemMykholap@users.noreply.github.com>
2020-06-24 10:04:30 +0300

    sda

--- js/stand-up/event/task3/index.js
+++ js/stand-up/event/task3/index.js
@@ -1,19 +1,6 @@
-// const buttons = document.querySelectorAll('.btn')
-
-// const clickHandler = (event) => {
-//     console.log(event.target.textContent)
-// };
-// buttons.forEach(el => el.addEventListener('click', clickHandler));
-
-
-// const listItem = document.querySelectorAll('.list-item');
 const list = document.querySelector('.list');
-
-
-
 clickHandler = (event) => {
     if (!event.target.classList.contains('list-item')) return;
     console.log(event.target.innerHTML)
 };
 list.addEventListener('click', clickHandler)
-    // listItem.forEach(el => el.addEventListener('click', clickHandler));
