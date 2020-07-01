38f14b3be7c697ad640c41cdcb79903598e7cc9d
ArtemMykholap <56889896+ArtemMykholap@users.noreply.github.com>
2020-07-01 10:30:51 +0300

    asdas

--- js/stand-up/ass/task1/index.js
+++ js/stand-up/ass/task1/index.js
@@ -1,24 +1,4 @@
-// const baseUrl = 'https://5e5cf5eb97d2ea0014796f01.mockapi.io/api/v1';
-
-// function getUserData(userId) {
-//     const response = await fetch(`${baseUrl}/users/${userId}`);
-//     then(response => {
-//         if (!response.ok) {
-//             throw new Error('Can not find a user');
-//         }
-//         return response.json();
-//     });
-// }
-
-// async function printUserData(userId) {
-//     const user = await getUserData(userId)
-
-
-// }
-
-// printUserData(10)
-
-const baseUrl = 'https://5e5cf5eb97d2ea0014796f01.mockapi.io/api/v1';
+const baseUrl = 'https://5e5cf5eb97d2ea0014796f01.mockap.io/api/v1';
 
 export async function getUserData(userId) {
     try {
@@ -42,3 +22,4 @@ export async function printUserData(userId) {
     }
 
 }
+printUserData(20)
