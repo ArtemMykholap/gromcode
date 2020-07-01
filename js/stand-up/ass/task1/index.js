5c60a79342fe3a767e41232bc096d871aabb578c
ArtemMykholap <56889896+ArtemMykholap@users.noreply.github.com>
2020-07-01 10:12:37 +0300

    fd

---
+++ js/stand-up/ass/task1/index.js
@@ -0,0 +1,47 @@
+const baseUrl = 'https://5e5cf5eb97d2ea0014796f01.mockapi.io/api/v1';
+
+function getUserData(userId) {
+    return fetch(`${baseUrl}/users/${userId}`).then(response => {
+        if (!response.ok) {
+            throw new Error('Can not find a user');
+        }
+        return response.json();
+    });
+}
+
+function printUserData(userId) {
+    getUserData(userId)
+        .then(userData => {
+            console.log(userData);
+        })
+        .catch(error => {
+            console.log(error.message);
+        });
+}
+
+printUserData(10)
+
+// const baseUrl = 'https://5e5cf5eb97d2ea0014796f01.mockap.io/api/v1';
+
+// export async function getUserData(userId) {
+//     try {
+//         const request = await fetch(`${baseUrl}/users/${userId}`);
+//         const response = await request;
+//         return response.json()
+//     } catch (error) {
+//         throw new Error('Can not find a user');
+
+//     }
+// }
+
+
+// export async function printUserData(userId) {
+//     try {
+//         const response = getUserData(userId);
+//         let request = await response;
+//         console.log(request)
+//     } catch (error) {
+//         console.log(error.message)
+//     }
+
+// }
